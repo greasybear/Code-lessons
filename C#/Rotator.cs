4c460dc43603942eb54881d779e3e5b1b7a8cce2
greasybear <captainfalconlink@hotmail.com>
2016-10-10 17:42:49 -0500

    Update Rotator.cs

--- C#/Rotator.cs
+++ C#/Rotator.cs
@@ -1,47 +1,10 @@
 using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-    public float speed;
-    public Text countText;
-    public Text winText;
-    private Rigidbody rb;
-    private int count;
+public class Rotator : MonoBehaviour {
 
-    void Start ()
+	void Update ()
     {
-        rb = GetComponent <Rigidbody>();
-        count = 0;
-        SetCountText ();
-        winText.text = "";
-    }
-	
-    void FixedUpdate ()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce (movement * speed);
-    }
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Pick Up"))
-        {
-            other.gameObject.SetActive(false);
-            count = count + 1;
-            SetCountText ();
-        }
-    }
-
-    void SetCountText()
-    {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 10)
-        {
-            winText.text = "You Win!";
-        }
-    }
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
 }
