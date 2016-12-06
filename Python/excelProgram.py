259e5329ae7829c68becea9390b9f7024760086e
greasybear <captainfalconlink@hotmail.com>
2016-12-06 10:00:49 -0600

    Update excelProgram.py

--- Python/excelProgram.py
+++ Python/excelProgram.py
@@ -6,10 +6,15 @@ file = 'c:/Users/Awillis/Desktop/Excel Trackers/PythonApp/testExcel.xls'
 
 df = pd.read_excel(file)
 
-df = df.drop(df.columns[37:52], ['Master Customer', 'Customer Type', 'Is IoT', 'List Price', 'Unit Price'], axis=1)
+df = df.drop(df.columns[37:52], axis=1)
+
+df = df.drop(['Master Customer', 'Customer Type', 'Is IoT', 'List Price', 'Unit Price'], axis=1)
 
 #this is where copying and pasting column I should go-should work???
-quant = df['Quantity'}
+quant=df['Quantity']
+
+df = df.drop('Quantity', 1)
+
 df.insert(21, 'Quantity', quant)
 
 new_col = df['Customer'].str.cat(df['Customer Number'], sep='-')
@@ -18,7 +23,12 @@ df = df.drop(['Customer','Customer Number'], axis=1)
 
 df.insert(5,'Customer',new_col)
 
-df['Quantity', 'Line Number', 'Order Number'] = pd.to_numeric(df['Quantity', 'Line Number', 'Order Number'])
+df['Order Date'] = pd.to_datetime(df['Order Date'], format = '%m%d%y')
 
-df['Order Date', 'Booking Date'] = pd.to_datetime(df['Order Date', 'Booking Date'], format = '%m%d%y')
+df['Order Number'] = pd.to_numeric(df['Order Number'])
 
+df['Line Number'] = pd.to_numeric(df['Line Number'])
+
+df['Quantity'] = pd.to_numeric(df['Quantity'])
+
+#df['BookingDate'] = pd.to_datetime(df['BookingDate'], format = '%m%d%y') this one doens't convert, probably because text format of cells
